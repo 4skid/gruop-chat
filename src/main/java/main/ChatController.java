d447d07f98a2b67642f25d5ed9dc58ce1690f126
Михаил Тукмаков <amgs630@gmail.com>
2022-10-18 23:56:16 +0300

    added repositories

--- src/main/java/main/ChatController.java
+++ src/main/java/main/ChatController.java
@@ -1,12 +1,57 @@
 package main;
 
+import main.model.MessageRepository;
+import main.model.User;
+import main.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class ChatController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MessageRepository messageRepository;
+
     @GetMapping("/init")
-    public String init(){
-        return "YES";
+    public HashMap<String, Boolean> init() {
+        HashMap<String, Boolean> response = new HashMap<>();
+        response.put("result", true);
+        return response;
+    }
+
+    @PostMapping("/auth")
+    public HashMap<String, Boolean> auth(@RequestParam String name) {
+        HashMap<String, Boolean> response = new HashMap<>();
+        String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
+        User user = new User();
+        user.setName(name);
+        user.setSessionId(sessionId);
+        userRepository.save(user);
+        response.put("result", true);
+        return response;
+    }
+
+    @PostMapping("/message")
+    public Boolean sendMessage(@RequestParam String message) {
+        return true;
+    }
+
+    @GetMapping("/message")
+    public List<String> getMessagesList() {
+        return null;
+    }
+
+    @GetMapping("/user")
+    public HashMap<Integer, String> getUsersList() {
+        return null;
     }
 }
