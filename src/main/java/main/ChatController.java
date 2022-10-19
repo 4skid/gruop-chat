3cf9525f0bd22a20bc7c113d4faecb74bea552bd
Михаил Тукмаков <amgs630@gmail.com>
2022-10-19 13:13:12 +0300

    added Dto

--- src/main/java/main/ChatController.java
+++ src/main/java/main/ChatController.java
@@ -1,35 +1,53 @@
 package main;
 
+import main.dto.DtoMessage;
+import main.dto.MessageMapper;
+import main.model.Message;
 import main.model.MessageRepository;
 import main.model.User;
 import main.model.UserRepository;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
-public class ChatController {
+public class ChatController
+{
     @Autowired
-    private UserRepository userRepository;
-
+    private  UserRepository userRepository;
     @Autowired
     private MessageRepository messageRepository;
 
+    private MessageMapper messageMapper;
+
     @GetMapping("/init")
-    public HashMap<String, Boolean> init() {
+    public HashMap<String, Boolean> init()
+    {
         HashMap<String, Boolean> response = new HashMap<>();
-        response.put("result", true);
+
+        Map<String, Boolean> result = new HashMap<>();
+        String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
+        Optional<User> userOpt = userRepository.findBySessionId(sessionId);
+
+        response.put("result", userOpt.isPresent());
         return response;
     }
 
     @PostMapping("/auth")
-    public HashMap<String, Boolean> auth(@RequestParam String name) {
+    public HashMap<String, Boolean> auth(@RequestParam String name)
+    {
         HashMap<String, Boolean> response = new HashMap<>();
         String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
         User user = new User();
@@ -41,17 +59,35 @@ public class ChatController {
     }
 
     @PostMapping("/message")
-    public Boolean sendMessage(@RequestParam String message) {
-        return true;
+    public Map<String, Boolean> sendMessage(@RequestParam String message)
+    {
+        if (Strings.isEmpty(message)) {
+            return Map.of("result", false);
+        }
+        String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
+        User user = userRepository.findBySessionId(sessionId).get();
+
+        Message msg = new Message();
+        msg.setDateTime(LocalDateTime.now());
+        msg.setMessage(message);
+        msg.setUser(user);
+        messageRepository.saveAndFlush(msg);
+        return Map.of("result", true);
     }
 
     @GetMapping("/message")
-    public List<String> getMessagesList() {
-        return null;
+    public List<DtoMessage> getMessagesList()
+    {
+        return messageRepository
+                .findAll(Sort.by(Sort.Direction.ASC, "dateTime"))
+                .stream()
+                .map(MessageMapper::map)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/user")
-    public HashMap<Integer, String> getUsersList() {
+    public HashMap<Integer, String> getUsersList()
+    {
         return null;
     }
 }
